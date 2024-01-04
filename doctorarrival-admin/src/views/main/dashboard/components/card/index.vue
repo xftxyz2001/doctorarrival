fd233e9d63389f57f88f8f09dd79c2ee65ec315c
通晓宇宙 <2581011320@qq.com>
2024-01-05 00:42:48 +0800

    未开放页面提示

--- doctorarrival-admin/src/views/main/dashboard/components/card/index.vue
+++ doctorarrival-admin/src/views/main/dashboard/components/card/index.vue
@@ -13,10 +13,10 @@ export default defineComponent({
   },
   setup() {
     const list = [
-      { id: 1, name: '访问人数', data: '100000', color: '#4e73df', icon: 'sfont system-yonghu' },
-      { id: 2, name: '最新信息', data: '30', color: '#1cc88a', icon: 'sfont system-xiaoxi' },
-      { id: 3, name: '库存数量', data: '20000', color: '#36b9cc', icon: 'sfont system-shuliang_mianxing' },
-      { id: 4, name: '当月营收', data: '20,000', color: '#f6c23e', icon: 'sfont system-jindutiaoshouyidaozhang' }
+      { id: 1, name: '注册用户', data: '100000', color: '#4e73df', icon: 'sfont system-yonghu' },
+      { id: 2, name: '接入医院', data: '30', color: '#1cc88a', icon: 'sfont system-xiaoxi' },
+      { id: 3, name: '订单总量', data: '20000', color: '#36b9cc', icon: 'sfont system-shuliang_mianxing' },
+      { id: 4, name: '订单金额', data: '20,000', color: '#f6c23e', icon: 'sfont system-jindutiaoshouyidaozhang' }
     ]
     return {
       list
--- doctorarrival-admin/src/views/main/sms/index.vue
+++ doctorarrival-admin/src/views/main/sms/index.vue
@@ -1,3 +1,19 @@
 <template>
-    404
+  <div>
+    <h1>消息管理</h1>
+  </div>
+
+  <!-- 按钮：返回首页 -->
+  <router-link to="/">
+    <el-button>返回首页</el-button>
+  </router-link>
 </template>
+
+<script setup>
+import { ElMessage } from 'element-plus';
+
+ElMessage({
+  message: '该功能暂未开放',
+  type: 'warning'
+})
+</script>
