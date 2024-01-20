9103eba65d98cc5c177b01924b8422701e449fa4
通晓宇宙 <2581011320@qq.com>
2024-01-20 20:51:12 +0800

    讨论页

--- doctorarrival-site/pages/user/discuss.vue
+++ doctorarrival-site/pages/user/discuss.vue
@@ -5,11 +5,15 @@
 
     <!-- 右侧 内容 -->
     <div class="page-container">
-      <div>尽请期待</div>
+      <vue-utterances repo="xftxyz2001/nwu-graduation-design" theme="github-light" issue-term="pathname" />
+
+      <div style="float: right">
+        <a href="https://github.com/khalby786/vue-utterances" target="_blank">Powered by utterances</a>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
-
-<style></style>
+<script setup>
+import VueUtterances from "vue-utterances";
+</script>
