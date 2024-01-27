bbedab47cf52ed796622859724fef76445dddfe4
通晓宇宙 <2581011320@qq.com>
2024-01-27 09:31:12 +0800

    fix ./mvnw: Permission denied

--- mock-hospital/build.sh
+++ mock-hospital/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd ../doctorarrival
+chmod +x mvnw
 ./mvnw clean install -DskipTests
 cd ../mock-hospital
+chmod +x mvnw
 ./mvnw clean package -DskipTests
 
 docker build -t xftxyz/mock:0.0.1 .
