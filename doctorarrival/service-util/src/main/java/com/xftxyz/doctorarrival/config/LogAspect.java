dd7d330184b733eb9894f7437067b8115bd8a63d
通晓宇宙 <2581011320@qq.com>
2024-04-09 09:01:12 +0800

    使用jackson替换tostring

--- doctorarrival/service-util/src/main/java/com/xftxyz/doctorarrival/config/LogAspect.java
+++ doctorarrival/service-util/src/main/java/com/xftxyz/doctorarrival/config/LogAspect.java
@@ -1,16 +1,19 @@
 package com.xftxyz.doctorarrival.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-import java.util.Arrays;
-
 @Slf4j
 @Aspect
+@RequiredArgsConstructor
 public class LogAspect {
+
+    private final ObjectMapper objectMapper;
 
     @Pointcut("execution(* com.xftxyz.doctorarrival.*.service.impl.*.*(..))")
     public void serviceMethod() {
@@ -28,8 +31,8 @@ public class LogAspect {
         String className = joinPoint.getTarget().getClass().getName();
         Object result = joinPoint.proceed();
         log.info("{}.{}\n入参: {}\n出参: {}\n" +
-                "----------------------------------------------------------------------------------------------------",
-                className, methodName, Arrays.toString(args), result);
+                 "----------------------------------------------------------------------------------------------------",
+                className, methodName, objectMapper.writeValueAsString(args), objectMapper.writeValueAsString(result));
         return result;
     }
 
