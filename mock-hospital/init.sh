1627d993e89a6de330b7e7e2061ec5a772b0e4f5
通晓宇宙 <2581011320@qq.com>
2024-04-09 20:55:02 +0800

    修改初始化策略

--- mock-hospital/init.sh
+++ mock-hospital/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir -p ~/appconfig
-docker run -p 8999:8999 --name mock -d xftxyz/mock:0.0.1
-docker cp mock:/app/config ~/appconfig/mock
-docker rm -f mock
+mkdir -p ~/appconfig/mock
+# docker run -p 8999:8999 --name mock -d xftxyz/mock:0.0.1
+# docker cp mock:/app/config ~/appconfig/mock
+# docker rm -f mock
+
+cp -r config/* ~/appconfig/mock
